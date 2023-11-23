312e5f76b64d2f6eb6c5935e050214bc3a017632
karlapa06 <A01705331@tec.mx>
2023-11-23 13:47:29 -0600

    create project

---
+++ ExamenM/ExamenM/ExamenMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenMApp.swift
+//  ExamenM
+//
+//  Created by KARLA PADILLA on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamenMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
